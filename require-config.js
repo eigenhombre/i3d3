23010a8794667e53d10855b4a75a4e66c3837a3d
Dean Radcliffe <dradcliffe@opinionlab.com>
2014-05-02 14:37:51 -0500

    Added example-require.html
    This shows the use of i3d3 in a require-js environment.

--- require-config.js
+++ require-config.js
@@ -1,10 +1,20 @@
 require.config({
+  urlArgs: "bust=" + (new Date()).getTime(),
   shim: {
-
+    d3: {
+      exports: "d3"
+    },
+    underscore: {
+      exports: "_"
+    }
   },
   paths: {
     i3d3: "i3d3",
     underscore: "bower_components/underscore/underscore",
     d3: "bower_components/d3/d3"
   }
+});
+
+require(["i3d3"], function(){
+  require(["i3d3-demo"]);
 });
